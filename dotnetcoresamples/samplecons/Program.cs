d4aa5c90a6e2b75296453f3747e2f52f9107c23d
Christian Nagel <christian@christiannagel.com>
2017-05-29 13:27:38 +0200

    .NET Core samples

---
+++ dotnetcoresamples/samplecons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace samplecons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
