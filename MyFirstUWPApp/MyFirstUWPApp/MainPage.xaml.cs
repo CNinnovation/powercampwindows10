15cbb83b6bce22551cc78133ebd7a64318384624
Christian Nagel <christian@christiannagel.com>
2017-05-29 13:28:39 +0200

    First uwp sample

---
+++ MyFirstUWPApp/MyFirstUWPApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MyFirstUWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            button1.Width = 500;
+            var gradientStops = new GradientStopCollection();
+            gradientStops.Add(new GradientStop() { Color = Colors.Blue, Offset = 0 });
+            gradientStops.Add(new GradientStop { Color = Colors.Red, Offset=1 });
+            var myBrush = new LinearGradientBrush(gradientStops, 90);
+
+            button1.Background = myBrush;
+        }
+    }
+}
