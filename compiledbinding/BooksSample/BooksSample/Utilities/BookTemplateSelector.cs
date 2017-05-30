037fdad9b0acdaa6737004c7b75f7da70f76b035
Christian Nagel <christian@christiannagel.com>
2017-05-30 16:17:08 +0200

    compiled binding

---
+++ compiledbinding/BooksSample/BooksSample/Utilities/BookTemplateSelector.cs
@@ -0,0 +1,41 @@
+﻿using BooksSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BooksSample.Utilities
+{
+    public class BookTemplateSelector : DataTemplateSelector
+    {
+        public BookTemplateSelector()
+        {
+
+        }
+        public DataTemplate DefaultBookTemplate { get; set; }
+        public DataTemplate WroxBookTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            DataTemplate selectedTemplate = null;
+            if (item is Book book)
+            {
+                switch (book.Publisher)
+                {
+                    case "Wrox Press":
+                        selectedTemplate = WroxBookTemplate;
+                        break;
+                    default:
+                        selectedTemplate = DefaultBookTemplate;
+                        break;
+                }
+            }
+
+            return selectedTemplate;
+        }
+
+    }
+}
---
+++ compiledbinding/BooksSample/BooksSample/Utilities/ObjectToObjectConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BooksSample.Utilities
+{
+    public class ObjectToObjectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => value;
+    }
+}
