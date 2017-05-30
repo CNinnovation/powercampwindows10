44b76cf4b151baee5372ff55823e66e7f2140fcb
Christian Nagel <christian@christiannagel.com>
2017-05-30 10:50:15 +0200

    controls sample

---
+++ controls/ControlsDemo/ControlsDemo/BooksRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlsDemo
+{
+    public class BooksRepository
+    {
+        private readonly List<Book> _books = new List<Book>();
+        public BooksRepository()
+        {
+            _books.AddRange(new Book[]
+            {
+                new Book { Title = "Professional C# 6", Publisher = "Wrox Press"},
+                new Book { Title = "Professional C# 5.0", Publisher = "Wrox Press"},
+                new Book { Title = "Enterprise Services", Publisher = "Addison Wesley"},
+            });
+        }
+        public IEnumerable<Book> GetBooks() => _books;
+            
+    }
+}
---
+++ controls/ControlsDemo/ControlsDemo/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ControlsDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var books = new BooksRepository().GetBooks();
+            this.DataContext = books;
+            //var b = new Book { Title = "Professional C# 6", Publisher = "Wrox Press" };
+            //button1.Content = b;
+            //button2.Content = b;
+
+        }
+
+        private async void OnClickButton(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MessageDialog("message");
+            await dialog.ShowAsync();
+        }
+    }
+}
