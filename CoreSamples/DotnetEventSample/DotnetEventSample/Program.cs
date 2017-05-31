5aebb4a228649d97cd09d65815a992b8d9181500
Christian Nagel <christian@christiannagel.com>
2017-05-31 14:46:13 +0200

    .net events

---
+++ CoreSamples/DotnetEventSample/DotnetEventSample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetEventSample
+{
+    class MyPublisher
+    {
+        // public event Action<string> MyEvent;
+        private Action<string> _myEvent;
+        public event Action<string> MyEvent
+        {
+            add => _myEvent += value;
+            remove => _myEvent -= value;
+        }
+
+        private void RaiseMyEvent(string message)
+        {
+            // _myEvent?.Invoke(message);
+            if (_myEvent != null)
+            {
+                foreach (Action<string> myEvent in _myEvent.GetInvocationList())
+                {
+                    try
+                    {
+                        myEvent.Invoke(message);
+                    }
+                    catch
+                    {
+                        // unregister - use for()
+                    }
+
+                }
+            }
+        }
+
+
+        public void Publish()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
