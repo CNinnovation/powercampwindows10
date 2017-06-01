be97f696fdce3ceab16cba1ac3b5d24b33f4faec
Christian Nagel <christian@christiannagel.com>
2017-06-01 15:57:09 +0200

    code fix

--- frank/EFCore/EFCore/Program.cs
+++ frank/EFCore/EFCore/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCore
 {
@@ -14,17 +15,17 @@ namespace EFCore
 
             using (var db = new BooksContext())
             {
-
+                bool created = db.Database.EnsureCreated();
                 // var context = new BooksContext();
 
                 // string t = context.Database.Connection.ConnectionString;
                 
                 // Neue DB anlegen
-                var book1 = new Books { BookID = 1, Author = "Hans Wurst", Title = "Dreams of a Butcher", Publisher = "Meat Press", Year = 1986 };
+                var book1 = new Books { Author = "Hans Wurst", Title = "Dreams of a Butcher", Publisher = "Meat Press", Year = 1986 };
                 db.Books.Add(book1);
-                var book2 = new Books { BookID = 2, Author = "Hans Wurst", Title = "Years spent with a Pork", Publisher = "Meat Press", Year = 1997 };
+                var book2 = new Books {  Author = "Hans Wurst", Title = "Years spent with a Pork", Publisher = "Meat Press", Year = 1997 };
                 db.Books.Add(book2);
-                var book3 = new Books { BookID = 3, Author = "Hans Wurst", Title = "Nothing else then Flesh", Publisher = "Meat Press", Year = 1969 };
+                var book3 = new Books { Author = "Hans Wurst", Title = "Nothing else then Flesh", Publisher = "Meat Press", Year = 1969 };
                 db.Books.Add(book3);
 
                 db.SaveChanges();
@@ -45,10 +46,17 @@ namespace EFCore
 
         public class BooksContext : DbContext
         {
-            public BooksContext():base(@"data source=(localdb)\MSSQLLocalDb;initial catalog=Books;trusted_connection=true;")
+            public BooksContext()
             {
 
             }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                base.OnConfiguring(optionsBuilder);
+                optionsBuilder.UseSqlServer(@"server=(localdb)\mssqllocaldb;database=BooksSample;trusted_connection=true");
+            }
+
             public DbSet<Books> Books { get; set; }
         }
 
