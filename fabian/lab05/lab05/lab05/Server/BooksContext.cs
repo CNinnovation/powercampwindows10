75347b648445c55477e88ae5a36581d1aa748df3
student2 <student2@vlab.etc.at>
2017-06-01 15:49:19 +0200

    db kram

---
+++ fabian/lab05/lab05/lab05/Server/BooksContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab05.Server
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<CBook> books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
