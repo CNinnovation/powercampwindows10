29d1237bf3f96b39b56dc092a765ff6499a39563
student1 <student1@PCMCSDW101>
2017-06-02 12:27:08 +0200

    Simple MVVM

---
+++ frank/Simple_MVVM/Simple_MVVM/MVVM/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_MVVM
+{
+    class DelegateCommand
+    {
+        private readonly Action _action;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        #pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+        #pragma warning restore 67
+    }
+}
